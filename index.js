786aa3613b24ccf88d2bb56e54c68355088bf5a4
Josh Ghent <me@joshghent.com>
2016-10-10 22:54:36 +0100

    Scaffolded out all unit tests.

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+exports.deepClone = function (data) {
+
+}
---
+++ test.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+var clone = require('./');
+
+describe('#deepClone', function () {
+    it ('can clone primitives', function () {
+
+    });
+
+    it ('can clone arrays', function () {});
+    it ('can clone objects', function () {});
+    it ('returns undefined when passed undefined', function () {});
+    it ('returns undefined when null is passed', function () {});
+    it ('can clone date objects', function () {});
+    it ('can clone arrays of arrays', function () {});
+    it ('can clone arrays of objects', function () {});
+    it ('can clone arrays filled with nulls and undefined', function () {});
+    it ('can clone objects filled with nulls and undefined', function () {});
+    it ('can clone objects with a nested arrays as a value', function () {});
+    it ('can clone regex', function () {});
+    it ('can clone functions', function () {});
+    it ('can clone promises', function () {});
+    it ('can clone NaN', function () {});
+    it ('can detect circular references', function () {});
+    it ('can clone symbols', function () {});
+});
