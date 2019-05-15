947370b0a8f14a36de91cdec383c868315529ee4
Josh Ghent <josh.ghent@cloudcall.com>
2019-05-15 10:28:46 +0100

    Added tests for symbols. Closes #3

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ var assert = require('assert');
 var deepClone = require('./bin/');
 
 describe('deepClone()', function () {
-    it ('can clone primitives', function () {
+    it('can clone primitives', function () {
         assert.deepEqual(deepClone(5), 5);
         assert.deepEqual(deepClone('test'), 'test');
     });
 
-    it ('can clone arrays', function () {
+    it('can clone arrays', function () {
         var arr = [1, 2, 3, 4, 5];
         var strArr = ['Hello', 'World'];
 
@@ -15,48 +15,48 @@ describe('deepClone()', function () {
         assert.deepEqual(deepClone(strArr), strArr);
     });
 
-    it ('can deeply cloning arrays', function () {
-        var arr = [[1,2,3,4], [5,6,7,8,9], [10,11,12,13,14]];
+    it('can deeply cloning arrays', function () {
+        var arr = [[1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]];
         var cloneArr = deepClone(arr);
         cloneArr[0] = ['a', 'b', 'c', 'd'];
 
         assert.notDeepEqual(cloneArr, arr);
     });
 
-    it ('can clone objects', function () {
-        var obj = {a: 0, b: 1, c: 2};
+    it('can clone objects', function () {
+        var obj = { a: 0, b: 1, c: 2 };
 
         assert.deepEqual(deepClone(obj), obj);
     });
 
-    it ('can deep clone objects', function () {
-        var obj = { a: 1, b: 0};
+    it('can deep clone objects', function () {
+        var obj = { a: 1, b: 0 };
         var copyObj = deepClone(obj);
         copyObj.b = 10;
         assert.notDeepEqual(copyObj, obj);
     });
 
-    it ('returns undefined when passed undefined', function () {
+    it('returns undefined when passed undefined', function () {
         assert.deepEqual(deepClone(undefined), undefined);
     });
 
-    it ('returns undefined when null is passed', function () {
+    it('returns undefined when null is passed', function () {
         assert.deepEqual(deepClone(null), undefined);
     });
 
-    it ('can clone date objects', function () {
+    it('can clone date objects', function () {
         var date = new Date();
 
         assert.deepEqual(deepClone(date), date);
     });
 
-    it ('can clone arrays of arrays', function () {
-        var arr = [[1,2,3,4], [5,6,7,8,9], [10,11,12,13,14]];
+    it('can clone arrays of arrays', function () {
+        var arr = [[1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]];
 
         assert.deepEqual(deepClone(arr), arr);
     });
 
-    it ('can clone arrays of objects', function () {
+    it('can clone arrays of objects', function () {
         var arr = [{
             id: 0,
             name: "Ronald McDonald",
@@ -70,32 +70,32 @@ describe('deepClone()', function () {
         assert.deepEqual(deepClone(arr), arr);
     });
 
-    it ('can clone arrays filled with nulls and undefined', function () {
+    it('can clone arrays filled with nulls and undefined', function () {
         var arr = [null, undefined, undefined, null];
 
         assert.deepEqual(deepClone(arr), arr);
     });
-    
-    it ('can clone objects filled with nulls and undefined', function () {
-        var obj = { undefined: 0, null: undefined, "": [undefined, null], 1: null};
+
+    it('can clone objects filled with nulls and undefined', function () {
+        var obj = { undefined: 0, null: undefined, "": [undefined, null], 1: null };
 
         assert.deepEqual(deepClone(obj), obj);
     });
-    
-    it ('can clone objects with a nested arrays as a value', function () {
+
+    it('can clone objects with a nested arrays as a value', function () {
         var obj = {
             a: [["things", 1, 2], ["hello", 6, 5, 4], ["world", 4, 31]],
             b: [["test"], ["more testing"]]
         }
-        
+
         assert.deepEqual(deepClone(obj), obj);
     });
-    
-    it ('can clone regex', function () {
+
+    it('can clone regex', function () {
         assert.deepEqual(deepClone(/foo/g), /foo/g);
     });
 
-    it ('can detect circular references', function () {
+    it('can detect circular references', function () {
         var a = {};
         var b = {};
         a.b = b;
@@ -103,4 +103,9 @@ describe('deepClone()', function () {
 
         assert.equal(deepClone(a), a);
     });
+
+    it('can clone symbols', () => {
+        let s = Symbol("foo");
+        assert.deepEqual(deepClone(s), s);
+    })
 });
